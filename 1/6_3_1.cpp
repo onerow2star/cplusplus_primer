3666b33ff3bfabea8d810fc3e964775ddfa58833
onerow2star <81426915+onerow2star@users.noreply.github.com>
2021-08-31 10:26:01 +0800

    update

---
+++ 1/6_3_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void r(int a)
+{
+    // return 0; // 返回值类型与函数类型不匹配
+}
+
+int main()
+{
+    return 0;
+}
---
+++ 1/6_3_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+const string &mainp()
+{
+    string ret;
+    ret = "23";
+    if (!ret.empty())
+        return ret; // error 
+    else 
+        return "Empty"; 
+        //该对象也是局部临时的对象 
+    // 这两条return语句都指向了不再可用的局部空间
+
+}
+
+int main()
+{
+    const string ps = mainp(); // error: taking address of temporary [-fpermissive
+    cout << ps << endl; 
+    Sleep(1);
+    cout << ps << endl; // 
+    return 0;
+}
